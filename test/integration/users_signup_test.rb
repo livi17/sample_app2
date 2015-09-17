fca3ae3eb0a21ff19cbdd22d4533c212122adb34
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-09-17 16:33:20 -0400

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+
   end
 
   test "valid signup information" do
@@ -24,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.nil?
   end
   
